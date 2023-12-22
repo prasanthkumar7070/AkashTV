331fbdba2f923e7703425facb8b4fdc91dcd50b7
Prasanth kumar <132974185+prasanthkumar7070@users.noreply.github.com>
2023-12-21 18:21:46 -1100

    init

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import TopNav from './NavSec/TopNav';
 import SideNav from './NavSec/SideNav';
 import ProfileTabs from './Mypage/ProfileTabs';
 import Footer from './Footer/Footer';
-import Tabs from './Components/Tabs';
-import Carousel from './Carousel'
+// import Tabs from './Components/Tabs';
+// import Carousel from './Carousel'
+// import Map from './Components/Map';
 
 function App() {
   return (
     <div>
       <TopNav />
       <SideNav />
-      <Carousel/>
+      {/* <Carousel/>
       <Tabs/>
-      {/* <ProfileTabs /> */}
+      <Map/> */}
+      <ProfileTabs />
       <Footer />
     </div>
   );
